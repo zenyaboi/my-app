1f78ff4de1356c63426b7471c56c21674c555685
zenyaboi <bannedboyinlife@gmail.com>
2021-08-06 14:55:04 -0300

    new MainNavegation file

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavegation from './components/layout/MainNavegation';
 
 function App() {
-  return <div>
-    <Switch>
-      <Route path='/' exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetupsPage />
-      </Route>
-      <Route path='/favorites'>
-        <FavoritesPage />
-      </Route>
-    </Switch>
-  </div>
+  return (
+    <div>
+      <MainNavegation />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
