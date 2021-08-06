ee756e501016651c02b274956882ba3991d31ea5
zenyaboi <bannedboyinlife@gmail.com>
2021-08-06 15:06:50 -0300

    new css file

--- src/components/layout/MainNavegation.js
+++ src/components/layout/MainNavegation.js
@@ -1,22 +1,26 @@
 import { Link } from 'react-router-dom';
 
+import classes from './MainNavegation.module.css';
+
 function MainNavegation() {
-    return <header>
-        <div>React Meetups</div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>All Meetups</Link>
-                    </li>
-                    <li>
-                        <Link to='/new-meetup'>New Meetup</Link>
-                    </li>
-                    <li>
-                        <Link to='/favorites'>My Favorites</Link>
-                    </li>
-                </ul>
-            </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>React Meetups</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to='/'>All Meetups</Link>
+                        </li>
+                        <li>
+                            <Link to='/new-meetup'>New Meetup</Link>
+                        </li>
+                        <li>
+                            <Link to='/favorites'>My Favorites</Link>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
+    );
 }
 
 export default MainNavegation;
