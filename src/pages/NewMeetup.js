1f29bcd12d857757034f4c66728be0b288f4b5de
Gabriel Franke <bannedboyinlife@gmail.com>
2021-08-11 15:15:36 -0300

    new code for NewMeetupForm and NewMeetup files

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+    function addMeetupHandler(meetupData) {
+        fetch (
+            'https://my-app-react-44cd6-default-rtdb.firebaseio.com/meetups.json', 
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
